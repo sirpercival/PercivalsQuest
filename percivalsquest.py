778cef42eaf6dfabbb7c4312d68198f500b30b0e
sirpercival <graymation@yahoo.com>
2014-01-06 18:00:36 -0500

    Pylinted

--- percivalsquest.py
+++ percivalsquest.py
@@ -1,3 +1,10 @@
+"""
+This is the main module for the Percival's Quest RPG.
+To start the game, at the command prompt type:
+
+prompt$ python percivalsquest.py
+"""
+
 #
 #  percivalsquest.py
 #  part of Percival's Quest RPG (duh)
@@ -19,8 +26,8 @@ o======o
 
 """
 
-from pq_rpg import *
-from pq_utilities import *
+import pq_rpg as pqr
+from pq_utilities import color, choose_from_list, confirm_quit
 import shelve, os, textwrap
 from colorama import init
 
@@ -33,37 +40,40 @@ def town(rpg):
             print "You are dead!"
             deadchar(rpg)
             break
-        print "Where would you like to go?",'\n', "Options: Home, Questhall, " \
-            "Shop, Shrine, or Dungeon [Level#] (max "+str(rpg.maxdungeonlevel)+")"
-        destinations = ["Dungeon", "Home", "Questhall", "Quest", "Shop", "Shrine"] + \
-            ["Dungeon "+str(i) for i in range(1,rpg.maxdungeonlevel+1)]
-        go = choose_from_list("Town> ",destinations,character=rpg.character,
-            rand=False,allowed=["sheet","help","equip"])
-        if go == "Home":
-            print "You hit the sack. Once you've annoyed all the bedbugs with " \
-                "your ineffectual fists, you lay down and sleep."
+        print "Where would you like to go?'\n'", "Options: Home, " \
+            "Questhall, Shop, Shrine, or Dungeon [Level#] (max "+ \
+            str(rpg.maxdungeonlevel)+")"
+        destinations = ["Dungeon", "Home", "Questhall", "Quest", \
+            "Shop", "Shrine"] + ["Dungeon "+str(i) for i in \
+            range(1, rpg.maxdungeonlevel+1)]
+        goto = choose_from_list("Town> ", destinations, rand=False,
+            character=rpg.character, allowed=["sheet","help","equip"])
+        if goto == "Home":
+            print "You hit the sack. Once you've annoyed all" \
+                "the bedbugs with your ineffectual fists, "\
+                "you lay down and sleep."
             rpg.character.sleep()
             rpg.save()
             continue
-        elif go in ["Questhall", "Quest"]:
+        elif goto in ["Questhall", "Quest"]:
             print "You head to the Questhall."
             rpg.questhall()
             continue
-        elif go == "Shop":
+        elif goto == "Shop":
             print "You head into the shop."
             rpg.visit_shop()
             continue
-        elif go == "Shrine":
+        elif goto == "Shrine":
             print "You head into the Shrine."
             rpg.visit_shrine()
             continue
         else:
-            go = go.split()
+            goto = goto.split()
             rpg.destination("start")
-            if go[0] == "Dungeon" and len(go) == 1:
+            if goto[0] == "Dungeon" and len(goto) == 1:
                 rpg.dungeonlevel = 1
             else:
-                rpg.dungeonlevel = int(go[1])
+                rpg.dungeonlevel = int(goto[1])
             print "You head into the Dungeon, level "+str(rpg.dungeonlevel)+"."
             dungeon(rpg)
             continue
@@ -72,36 +82,41 @@ def dungeon(rpg):
     """Maintain interaction with the dungeon"""
     if rpg.character.dead:
         return
-    do = ""
-    while do != "Leave":
-        actions = ["Explore","Leave"] if rpg.whereareyou == "start" else ["Explore","Backtrack"]
+    dothis = ""
+    while dothis != "Leave":
+        actions = ["Explore","Leave"] if rpg.whereareyou == "start" \
+            else ["Explore","Backtrack"]
         print "You're in the dank dark dungeon. What do you want to do?", \
             '\n', "Options: "+", ".join(actions)
-        do = choose_from_list("Dungeon> ",actions,character=rpg.character,
-            rand=False,allowed=["sheet","help","equip"])
-        if do == "Leave":
+        dothis = choose_from_list("Dungeon> ", actions, rand=False,
+            character=rpg.character, allowed=["sheet","help","equip"])
+        if dothis == "Leave":
             if rpg.whereareyou != "start":
-                print "You can't leave from here; you have to backtrack to the start of the level."
-                do = ""
+                print "You can't leave from here; you have to backtrack " \
+                    "to the start of the level."
+                dothis = ""
                 continue
             else:
                 rpg.destination("town")
                 print "You head back to town."
                 continue
-        elif do == "Backtrack":
+        elif dothis == "Backtrack":
             if rpg.whereareyou == "start":
-                print "You're already at the beginning of the level, Captain Redundant."
+                print "You're already at the beginning of the level, " \
+                    "Captain Redundant."
                 continue
             if rpg.check_backtrack():
-                print "You successfully find your way back to the beginning of the level."
+                print "You successfully find your way back " \
+                    "to the beginning of the level."
                 rpg.destination("start")
                 continue
             else:
-                print "On your way back, you get lost! You find yourself in another room of the dungeon..."
+                print "On your way back, you get lost! You find yourself " \
+                    "in another room of the dungeon..."
                 rpg.destination("dungeon")
                 rpg.explore()
                 continue
-        elif do == "Explore":
+        elif dothis == "Explore":
             rpg.destination("dungeon")
             rpg.explore()
             if rpg.character.dead:
@@ -111,22 +126,23 @@ def dungeon(rpg):
         
 def deadchar(rpg):
     """Deal with character death"""
-    print "What would you like to do? Options: Generate (a new character), Load, Quit"
-    do = choose_from_list("Dead> ",["Generate","Load","Quit"],character=rpg.character,
-        rand=False,allowed=["sheet","help"])
-    if do == "Quit":
+    print "What would you like to do? Options: "\
+        "Generate (a new character), Load, Quit"
+    dothis = choose_from_list("Dead> ", ["Generate", "Load", "Quit"],
+        character=rpg.character, rand=False, allowed=["sheet", "help"])
+    if dothis == "Quit":
         confirm_quit()
         deadchar(rpg)
         return
-    if do == "Load":
-        d = shelve.open(os.path.expanduser('data/pq_saves.db'))
-        rpg = d[rpg.player_name]
-        d.close()
+    if dothis == "Load":
+        save_db = shelve.open(os.path.expanduser('data/pq_saves'))
+        rpg = save_db[rpg.player_name]
+        save_db.close()
         print "Game successfully loaded!", "You begin in the town square."
         rpg.character.tellchar()
         town(rpg)
         return
-    if do == "Generate":
+    if dothis == "Generate":
         rpg = generate(rpg)
         print "You begin in the town square."
         town(rpg)
@@ -134,32 +150,36 @@ def deadchar(rpg):
         
 def generate(rpg):
     """Wrapper for making a new character"""
-    print "Time to make a new character! It'll be saved under this player name. Here we go!"
+    print "Time to make a new character! It'll be saved under this player name."
     rpg.character.chargen(rpg.player_name)
     rpg.save()
-    msg = "In the town of North Granby, the town militia has recently discovered that the plague of monsters "\
-        "harrassing the townspeople originates from a nearby dungeon crammed with nasties. " \
-        "As the resident adventurer, the Mayor of North Granby (a retired adventurer by the name of Sir Percival)" \
-        " has recruited you to clear out the dungeon."
+    msg = "In the town of North Granby, the town militia has recently " \
+        "discovered that the plague of monsters harrassing the townspeople " \
+        "originates from a nearby dungeon crammed with nasties. As the " \
+        "resident adventurer, the Mayor of North Granby (a retired " \
+        "adventurer by the name of Sir Percival) has recruited you to clear " \
+        "out the dungeon."
     print textwrap.fill(msg)
     return rpg
 
 def main():
     """Initialize and start the game session"""
     print logo
-    print textwrap.fill("Welcome to Percival's Quest! This is a solo (as of yet) random dungeoncrawl rpg written " \
-        "in python by the ever-resourceful (and extremely humble) sirpercival.")
-    player_name = raw_input(color.BOLD+"Please enter your player name> "+color.END)
+    print textwrap.fill("Welcome to Percival's Quest! This is a solo random " \
+        "dungeoncrawl rpg written in python by the ever-resourceful (and " \
+        "extremely humble) sirpercival.")
+    player_name = raw_input(color.BOLD+"Please enter your player name> "+
+        color.END)
     msg = "Welcome, "+player_name+"!"
-    rpg_instance = PQ_RPG(player_name)
-    d = shelve.open(os.path.expanduser('data/pq_saves.db'))
+    rpg_instance = pqr.PQ_RPG(player_name)
+    save_db = shelve.open(os.path.expanduser('data/pq_saves'))
     newgame = False
-    if player_name in d:
+    if player_name in save_db:
         msg += " You currently have a game saved. Would you like to load it?"
         print msg
         loadit = raw_input("Load (y/n)> ")
-        if loadit.lower() in ["y","yes","load"]:
-            rpg_instance = d[player_name]
+        if loadit.lower() in ["y", "yes", "load"]:
+            rpg_instance = save_db[player_name]
             print "Game successfully loaded."
             rpg_instance.character.tellchar()
         else:
@@ -167,12 +187,13 @@ def main():
     else:
         print msg + " You don't have a character saved..."
         newgame = True
-    d.close()
+    save_db.close()
     if newgame:
         rpg_instance = generate(rpg_instance)
     msg = "You begin in the town square. \n"
-    msg += "(Please note that at almost any prompt, you can choose Sheet to look at your charsheet, " \
-        "Equip to change your equipment, Help to enter the help library, or Quit to quit.)"
+    msg += "(Please note that at almost any prompt, you can choose Sheet " \
+        "to look at your charsheet, Equip to change your equipment, Help " \
+        "to enter the help library, or Quit to quit.)"
     print textwrap.fill(msg)
     rpg_instance.telltown()
     town(rpg_instance)
